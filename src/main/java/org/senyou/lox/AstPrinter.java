81ca6aff928027eb4bbd5e0b7a031b300638441d
senyoudev <younesmeskafe2018@gmail.com>
2024-09-16 23:02:54 +0100

    visitor pattern

---
+++ src/main/java/org/senyou/lox/AstPrinter.java
@@ -0,0 +1,4 @@
+package org.senyou.lox;
+
+public class AstPrinter {
+}
--- src/main/java/org/senyou/lox/Expr.java
+++ src/main/java/org/senyou/lox/Expr.java
@@ -3,6 +3,16 @@ package org.senyou.lox;
 import java.util.List;
 
 abstract class Expr {
+
+    abstract <R> R accept(Visitor<R> visitor);
+
+    interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
+
     static class Binary extends Expr {
         final Expr left;
         final Token operator;
@@ -12,17 +22,32 @@ abstract class Expr {
             this.operator = operator;
             this.right = right;
         }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
     }
     static class Grouping extends Expr {
         final Expr expression;
         Grouping(Expr expression) {
             this.expression = expression;
         }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
     }
     static class Literal extends Expr {
         final Object value;
         Literal(Object value) {
             this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
         }
     }
     static class Unary extends Expr {
@@ -31,6 +56,11 @@ abstract class Expr {
         Unary(Token operator, Expr right) {
             this.operator = operator;
             this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
         }
     }
 }
